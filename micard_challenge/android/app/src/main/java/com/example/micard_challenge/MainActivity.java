de5354aa2200432d4fd0f51c0b7badd34cf965fb
Prateek Aher <prateeksaher@gmail.com>
2021-01-28 00:01:09 +0530

    MiCard Challenge added BackgroundImage added to CircleAvatar()

---
+++ micard_challenge/android/app/src/main/java/com/example/micard_challenge/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.micard_challenge;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ micard_challenge/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage:
+                    AssetImage('images/IMG_20190613_032849_066.jpg'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
