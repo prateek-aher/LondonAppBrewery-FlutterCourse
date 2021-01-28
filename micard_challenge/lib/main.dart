71bdf782f26ce18de7d088ab4ab19bc4fce5bfc8
Prateek Aher <prateeksaher@gmail.com>
2021-01-28 16:43:19 +0530

    Mi Card challenge complete

--- micard_challenge/lib/main.dart
+++ micard_challenge/lib/main.dart
@@ -11,36 +11,82 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Column(
-              children: <Widget>[
-                CircleAvatar(
-                  radius: 50,
-                  backgroundImage:
-                      AssetImage('images/IMG_20190613_032849_066.jpg'),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage:
+                    AssetImage('images/IMG_20190613_032849_066.jpg'),
+              ),
+              Text(
+                'Prateek Aher',
+                style: TextStyle(
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Pacifico',
                 ),
-                Text(
-                  'Prateek Aher',
-                  style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontFamily: 'Pacifico',
+              ),
+              Text(
+                'FLUTTER DEVELOPER',
+                style: TextStyle(
+                  fontFamily: 'SourceSansPro',
+                  color: Colors.teal.shade100,
+                  fontSize: 20,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 91755 90989',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSansPro',
+                    ),
                   ),
                 ),
-                Text(
-                  'FLUTTER DEVELOPER',
-                  style: TextStyle(
-                    fontFamily: 'SourceSansPro',
-                    color: Colors.teal.shade100,
-                    fontSize: 20,
-                    letterSpacing: 2.5,
-                    fontWeight: FontWeight.bold,
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(
+                  vertical: 10,
+                  horizontal: 25,
+                ),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'prateeksaher@gmail.com',
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Colors.teal[900],
+                      fontFamily: 'SourceSansPro',
+                    ),
                   ),
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
